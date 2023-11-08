e8e8a08f2b93e7f7b1c1d7c9e738f7260723afca
kauã Chavez <kauakbkchavez@gmail.com>
2023-11-08 10:32:54 -0300

    UPDATE - CRUD Básico

--- blog/forms.py
+++ blog/forms.py
@@ -30,6 +30,22 @@ class PostModelForm(forms.ModelForm):
         
         if pub_date > datetime.today(): # exemplo para demonstrar a validação
             self.add_error(
-                'pub_date',
-                forms.ValidationError('Não é permitido datas futuras')
+                'pub_date', forms.ValidationError('Não é permitido datas futuras')
             )
+
+     def __init__(self, *args, **kwargs):
+        super(PostModelForm, self).__init__(*args, **kwargs)
+
+        # Configura um valor inicial para o campo pub_date
+        self.fields["pub_date"].initial = datetime.today()
+
+    class Meta:  # é uma classe mas está dentro do PostModelForm
+        # Vincula o model Post ao Form e define os campos a exibir
+        model = Post
+        fields = ("body_text", "pub_date", "categoria")
+        widgets = {
+            "pub_date": forms.widgets.DateInput( format='%Y-%m-%d',
+            attrs={"type": "date"}),
+            "categoria": forms.RadioSelect(),
+        }
+        labels = {"body_text": "", "categoria": "Assunto"} 
